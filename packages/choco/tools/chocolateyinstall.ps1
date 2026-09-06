40bef8b00163879f05477fba4e1c023c777ef254
arvaidasre <arvrekis@gmail.com>
2026-09-06 23:02:12 +0300

    Packages: update choco/winget to v1.9.21 with new hashes and ProductCode

--- packages/choco/tools/chocolateyinstall.ps1
+++ packages/choco/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop' # stop on all errors
 # Community release assets. Update the version below with each release
 # and refresh the checksum (choco install --download-checksum behavior).
-$url        = 'https://github.com/arvaidasre/Shell/releases/download/v1.9.19/Shell-1.9.19-setup-x64.msi'
+$url        = 'https://github.com/arvaidasre/Shell/releases/download/v1.9.21/Shell-1.9.21-setup-x64.msi'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'msi'
   url           = $url
   softwareName  = 'Nilesoft Shell'
-  checksum      = '9EB31DA31E70A7DE930528359134BDDC25463ECF2F85638950B0052FB93661C9'
+  checksum      = '5EFF5C4DFA44C0F083E944868F0B5512C42947211BBA1FD0F6B57ED70EC64B23'
   checksumType  = 'sha256'
   silentArgs   = '/qn /norestart'
   validExitCodes= @(0, 1641, 3010)
